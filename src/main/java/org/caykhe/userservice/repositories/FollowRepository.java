2a5f34afe183a5eb652732fae045860120a7e2a2
ThanhQuoc0719 <thanhquocnguyen1907@gmail.com>
2024-01-21 01:01:41 +0700

    add followRepository

---
+++ src/main/java/org/caykhe/userservice/repositories/FollowRepository.java
@@ -0,0 +1,27 @@
+package org.caykhe.userservice.repositories;
+
+import org.caykhe.userservice.models.Follow;
+import org.caykhe.userservice.models.FollowId;
+import org.caykhe.userservice.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FollowRepository extends JpaRepository<Follow, FollowId> {
+    Page<Follow> findAllByFollowed(User followed, Pageable pageable);
+
+    Page<Follow> findAllByFollower(User follower, Pageable pageable);
+
+    Optional<Follow> findByFollowerAndFollowed(User follower, User followed);
+
+    Optional<List<Follow>> findByFollower(User follower);
+
+    List<Follow> findByFollowed(User follower);
+
+    int countByFollowed(User user);
+
+    int countByFollower(User user);
+}

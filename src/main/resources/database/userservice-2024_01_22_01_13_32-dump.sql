d24d63a99329de155eda61c429a178919f653db5
Luong <luongnguyen.270902@gmail.com>
2024-01-22 01:21:52 +0700

    update

---
+++ src/main/resources/database/userservice-2024_01_22_01_13_32-dump.sql
@@ -0,0 +1,114 @@
+-- MySQL dump 10.13  Distrib 8.0.35, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: userservice
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `authentications`
+--
+
+DROP TABLE IF EXISTS `authentications`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `authentications` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `refresh_token` varchar(200) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_i2g7gfbs6hf03r304d6lv8hvj` (`username`),
+  CONSTRAINT `FKp78h4mwju0x0yd7pgndruc13t` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `authentications`
+--
+
+LOCK TABLES `authentications` WRITE;
+/*!40000 ALTER TABLE `authentications` DISABLE KEYS */;
+INSERT INTO `authentications` VALUES (3,'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsdW9uZ25ndXllbiIsImlhdCI6MTcwNTg1OTE4NSwiZXhwIjoxNzA4NDUxMTg1fQ.CDnso5ohBm_pz6BGsEsZfWsNCILWOiqUp2s1srPauqg','luongnguyen');
+/*!40000 ALTER TABLE `authentications` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `follows`
+--
+
+DROP TABLE IF EXISTS `follows`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `follows` (
+  `follower` varchar(50) NOT NULL,
+  `followed` varchar(50) NOT NULL,
+  PRIMARY KEY (`followed`,`follower`),
+  KEY `FKjnqt4f5bti6niw7afunse4de7` (`follower`),
+  CONSTRAINT `FKjkcxs3b09wycopcoihen12a5g` FOREIGN KEY (`followed`) REFERENCES `users` (`username`),
+  CONSTRAINT `FKjnqt4f5bti6niw7afunse4de7` FOREIGN KEY (`follower`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `follows`
+--
+
+LOCK TABLES `follows` WRITE;
+/*!40000 ALTER TABLE `follows` DISABLE KEYS */;
+/*!40000 ALTER TABLE `follows` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `avatar_url` varchar(100) DEFAULT NULL,
+  `bio` longtext,
+  `birthdate` date DEFAULT NULL,
+  `display_name` varchar(100) DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `gender` bit(1) DEFAULT NULL,
+  `password` varchar(200) NOT NULL,
+  `role` enum('ROLE_member','ROLE_admin') NOT NULL,
+  `username` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_6dotkott2kjsp8vw4d0m25fb7` (`email`),
+  UNIQUE KEY `UK_r43af9ap4edm43mmtq01oddj6` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (3,NULL,NULL,NULL,'Nguyễn Văn lương','user0002@gmail.com',NULL,'$2a$10$mUAukqbUM1Su5UJc5ncL/.RoHN3XHx.G2tAAzSsnGgENCD4NF71m6','ROLE_member','luongnguyen');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-01-22  1:13:32
